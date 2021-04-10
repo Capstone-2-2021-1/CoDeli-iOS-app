3a9a1152e242f8de81de0ad6f77f0a0f4c15dd2a
sspog <sspog.lim@gmail.com>
2021-04-11 00:46:33 +0900

    SignIn View (SwiftUI)

---
+++ CoDeli/CoDeli/CoDeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoDeliApp.swift
+//  CoDeli
+//
+//  Created by Changsung Lim on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CoDeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoDeli/CoDeli/ContentView.swift
@@ -0,0 +1,112 @@
+//
+//  ContentView.swift
+//  CoDeli
+//
+//  Created by Changsung Lim on 4/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+
+    @State private var isEditing = false
+
+    var body: some View {
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
+            Image("logo_title")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 400, height: 300, alignment: .center)
+
+            TextField(
+                "username or email",
+                text: $username
+            ) { isEditing in
+                self.isEditing = isEditing
+            } onCommit: {
+    //            validate(name: username)
+            }
+//            .frame(width: 400, height: 50)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(hex: 0xEAEAEA))
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2))
+            .padding()
+
+            TextField(
+                "password",
+                text: $password
+            ) { isEditing in
+                self.isEditing = isEditing
+            } onCommit: {
+    //            validate(name: username)
+            }
+//            .frame(width: 400, height: 50)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(hex: 0xEAEAEA))
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2))
+            .padding()
+
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                Button(action: {
+                    print("로그인 버튼 눌림!")
+                }) {
+                    Text("로그인")
+                        .padding()
+                }
+                .frame(width: 400, height: 50)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: 0x008BBA, alpha: 0.7))
+                        .shadow(color: .gray, radius: 2, x: 0, y: 2))
+                .foregroundColor(.white)
+                .font(.title2)
+                .padding()
+
+                Button(action: {
+                    print("회원가입 버튼 눌림!")
+                }) {
+                    Text("회원가입")
+                        .padding()
+                }
+                .frame(width: 400, height: 50)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: 0xFFFFFF))
+                        .shadow(color: .gray, radius: 2, x: 0, y: 2))
+                .foregroundColor(Color(hex: 0x008BBA, alpha: 0.7))
+                .font(.title2)
+                .padding()
+            })
+
+        })
+        .padding()
+        .autocapitalization(.none)
+        .disableAutocorrection(true)
+//        .textFieldStyle(RoundedBorderTextFieldStyle())
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
