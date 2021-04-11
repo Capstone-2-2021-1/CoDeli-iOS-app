ab334403dc6e35238ae5d28211af6a937ed07392
sspog <sspog.lim@gmail.com>
2021-04-11 23:16:32 +0900

    HomeView (SwiftUI) - Add makeRoomButton

--- CoDeli/CoDeli/Views/HomeView.swift
+++ CoDeli/CoDeli/Views/HomeView.swift
@@ -10,8 +10,24 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            List(rooms) { room in
-                HomeRow(room: room)
+            ZStack {
+                List(rooms) { room in
+                    HomeRow(room: room)
+                }
+
+                VStack {
+                    Spacer()    // minLength?
+
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            print("방 만들기 버튼 눌림")
+                        }) {
+                            Image("makeRoomButton")
+                        }
+                        .shadow(color: Color/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 3, x: 3, y: 3)
+                    }
+                }
             }
             
             .navigationBarTitle("서울 동작구 흑석로 84", displayMode: .inline)
