df9100a2f5a583770e215772896d1a1c9e3ae15d
sspog <sspog.lim@gmail.com>
2021-05-08 20:45:35 +0900

    Send response.status & .expirationTime (Firebase)
    Remove PaymentView.swift
        -> struct PaymentFullScreenModalView (RoomDetailView.swift)

--- CoDeli/CoDeli/Views/ContentView.swift
+++ CoDeli/CoDeli/Views/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
 
     var body: some View {
         // for debug - 로그인 생략
-        return AnyView(MyQRView())
+        return AnyView(MainView())
 //        if info.isSignIn == .success {
 //            return AnyView(MainView())
 //        } else {
--- CoDeli/CoDeli/Views/RoomDetailView.swift
+++ CoDeli/CoDeli/Views/RoomDetailView.swift
@@ -7,9 +7,128 @@
 
 import SwiftUI
 import Firebase
+import KlipSDK
+
+struct PaymentFullScreenModalView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    @EnvironmentObject var realtimeData: RealtimeData
+    var ref: DatabaseReference! = Database.database().reference()
+
+    var room: Room
+    @Binding var menuName: String
+    @Binding var menuPrice: String
+    @Binding var status: Bool
+
+    @State private var orderCost: UInt = 6100
+    @State private var deliveryCost: UInt = 500
+    @State private var totalCost: UInt = 6600
+    @State private var totalKlay: Double = 1.9270072    // 소수점 아래 6자리까지?
+    @State private var klayMarketPrice: UInt = 3425
+    @State private var stdTime: String = "2021/03/25 16:30"
+
+    var body: some View {
+        VStack {
+            Spacer()
+
+            Text("₩\(orderCost) + ₩\(deliveryCost) (배달팁)")
+                .foregroundColor(Color(hex: 0x707070))
+            Text("총 ₩\(totalCost)")
+                .foregroundColor(Color(hex: 0x193154))
+                .font(.title)
+            Image("klaytn")
+            Text("총 \(totalKlay) KLAY")
+                .foregroundColor(Color(hex: 0x193154))
+                .font(.system(size: 30, weight: .heavy))
+            Text("(1KLAY = ₩\(klayMarketPrice), \(stdTime) 기준)")
+                .foregroundColor(Color(hex: 0x707070))
+
+            Spacer()
+
+            Button(action: {
+                print("결제 버튼 눌림!")
+
+                // server의 지갑 주소 가져오기
+                realtimeData.fetchServerWalletAddress()
+                let toWalletAddress = realtimeData.serverWalletAddress
+
+                let klip = KlipSDK.shared
+                let bappInfo: BAppInfo = BAppInfo(name : "CoDeli")
+                var myRequestKey: String = ""
+
+                // KLAY 전송 트랜잭션 요청문
+                let req: KlayTxRequest = KlayTxRequest(to: "0x697e67f7767558dcc8ffee7999e05807da45002d", amount: "0.01")
+
+                // prepare
+                klip.prepare(request: req, bappInfo: bappInfo) { result in
+                    switch result {
+                    case .success(let response):
+                        print("*klip.prepare.success")
+                        print(response)
+                        myRequestKey = response.requestKey
+                    case .failure(let error):
+                        print("*klip.prepare.failure")
+                        print(error)
+                    }
+                }
+
+                // 위의 prepare에서 response.requestKey가 myRequestKey에 바로 들어오지 않음.. 일단 임시 방편으로..
+                while true {
+                    if myRequestKey != "" {
+                        break
+                    }
+                }
+
+                // request - 카카오톡의 Klip앱으로 이동
+                klip.request(requestKey: myRequestKey)
+
+                // getResult
+                klip.getResult(requestKey: myRequestKey) { result in
+                    switch result {
+                    case .success(let response):
+                        print("*klip.getResult.success")
+                        print(response)
+
+                        ref.child("Chat/\(room.id)/partitions/\(realtimeData.myInfo.nickname)").setValue(
+                            ["id": realtimeData.myInfo.nickname,
+                             "menu_name": menuName,
+                             "menu_price": UInt(menuPrice) ?? 0,    // 위험..
+                             "status": status,
+                             "sendingStatus": response.status,
+                             "expiration_time": response.expirationTime]
+                        )
+
+                        // 기존의 Firebase 데이터를 다 삭제해버림..
+//                        let post = ["sendingStatus": response.status, "expiration_time": response.expirationTime] as [String : Any]
+//                        let childUpdates = ["Chat/\(room.id)/partitions/\(realtimeData.myInfo.nickname)": post] as [String : Any]
+//                        ref.updateChildValues(childUpdates)
+
+                        presentationMode.wrappedValue.dismiss()
+                    case .failure(let error):
+                        print("*klip.getResult.failure")
+                        print(error)
+                    }
+                }
+            }) {
+                Text("결제하기")
+                    .padding()
+            }
+            .frame(width: 400, height: 50)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(hex: 0x193154))
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2))
+            .foregroundColor(.white)
+            .font(.title2)
+        }
+    }
+}
 
 struct RoomDetailView: View {
+    @State private var isPresented = false
+
     @EnvironmentObject var realtimeData: RealtimeData
+    var ref: DatabaseReference! = Database.database().reference()
 
     var room: Room
 
@@ -33,27 +152,43 @@ struct RoomDetailView: View {
                 .font(.system(size: 21, design: .rounded))
 
                 Spacer()
-                Button(isReady ? "취소" : "준비") {
-                    isReady = isReady ? false : true // flip
-                    status = isReady ? true : false
 
-                    var ref: DatabaseReference!
-                    ref = Database.database().reference()
-                    ref.child("Chat/\(room.id)/partitions/\(realtimeData.myInfo.nickname)").setValue(
-                        ["id": realtimeData.myInfo.nickname,
-                         "menu_name": menuName,
-                         "menu_price": UInt(menuPrice) ?? 0,    // 위험..
-                         "status": status]
-                    )
-                    hideKeyboard()
+                VStack {
+                    Button(isReady ? "취소" : "준비") {
+                        isReady = isReady ? false : true // flip
+                        status = isReady ? true : false
+
+                        ref.child("Chat/\(room.id)/partitions/\(realtimeData.myInfo.nickname)").setValue(
+                            ["id": realtimeData.myInfo.nickname,
+                             "menu_name": menuName,
+                             "menu_price": UInt(menuPrice) ?? 0,    // 위험..
+                             "status": status]
+                        )
+
+                        hideKeyboard()
+                    }
+                    .frame(width: 80, height: 40)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(hex:0xf6cd53))
+                            .shadow(color: .gray, radius: 2, x: 0, y: 2))
+                    .foregroundColor(.white)
+                    .font(.system(.headline, design: .rounded))
+                    .padding(.bottom, 5)
+
+                    Button("결제") {
+                        isPresented.toggle()
+                    }
+                    .fullScreenCover(isPresented: $isPresented) {
+                        PaymentFullScreenModalView(room: room, menuName: $menuName, menuPrice: $menuPrice, status: $status)
+                    }
+                    .frame(width: 80, height: 40)
+                    .background(RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color(hex:0x193154))
+                                    .shadow(color: .gray, radius: 2, x: 0, y: 2))
+                    .foregroundColor(.white)
+                    .font(.system(.headline, design: .rounded))
                 }
-                .frame(width: 80, height: 80)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color(hex:0xf6cd53))
-                        .shadow(color: .gray, radius: 2, x: 0, y: 2))
-                .foregroundColor(.white)
-                .font(.system(.title, design: .rounded))
             }
             .padding(5)
 
@@ -117,8 +252,6 @@ struct RoomDetailView: View {
                         let minute = calendar.component(.minute, from: date)
                         let second = calendar.component(.second, from: date)
 
-                        var ref: DatabaseReference!
-                        ref = Database.database().reference()
                         ref.child("Chat/\(room.id)/chat/\(realtimeData.myInfo.nickname)\(hour)\(minute)\(second)").setValue(
                             ["message": message,
                              "name": realtimeData.myInfo.nickname,
